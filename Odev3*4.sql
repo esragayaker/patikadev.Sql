e57290657d3e1622b3d5e0e462be147c16b6185c
Esra Gayaker <106603047+esragayaker@users.noreply.github.com>
2022-06-26 20:52:20 +0300

    Update Odev3*4.sql

--- Odev3*4.sql
+++ Odev3*4.sql
@@ -4,4 +4,4 @@
 --rental_rate 2.99 olan verileri sıralayınız.
 
 SELECT * FROM film
-WHERE title like 'C%' AND length > 90 AND rental_rate = 2.99
+WHERE title like 'C%' AND LENGTH(title) > 90 AND rental_rate = 2.99
